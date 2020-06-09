e5e1e906dd50aff0600edac39c1cd9be5937a886
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-09 16:50:26 -0700

    Created getStories function. Added API Key to function. Fetched API data from NewsAPI. Console logged API data. Put comment reminding to setState. Added getStories function to useEffect method.

--- src/components/TopStories.js
+++ src/components/TopStories.js
@@ -5,8 +5,18 @@ function TopStories() {
   const [] = useState([]);
 
   useEffect(() => {
-    //   someFunction();
+    getStories();
   }, []);
+
+  const getStories = async () => {
+    const API_KEY = '24c2c6eb3889484bbc95889c82c924b1';
+    const response = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+    );
+    const data = await response.json();
+    console.log(data.articles);
+    // setState(data.articles);
+  };
 
   return (
     <div>
