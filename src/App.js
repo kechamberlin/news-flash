11347b051e4d018396e84b18cf6691e2590d7129
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-09 16:15:04 -0700

    Created a new component file called TopStories. Imported and rendered TopStories component into App.js.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import TopStories from './components/TopStories';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <TopStories />
     </div>
   );
 }
---
+++ src/components/TopStories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TopStories() {
+  return (
+    <div>
+      <h1>Top Stories</h1>
+    </div>
+  );
+}
+
+export default TopStories;
