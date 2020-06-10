e341100f2ce68c991415a220bb6203ea033553bb
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-10 11:56:38 -0700

    Created Article component that destructures the title, abstract, image, and url properties in a basic format (likely to be added in a bootstrap card later).

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-function Article() {
-    return (
-        <div>
-            
-        </div>
-    )
+function Article({ title, abstract, image, url }) {
+  return (
+    <div>
+      <h4>{title}</h4>
+      <a href={url} target='_blank'>
+        <img
+          src={image}
+          alt='something'
+          style={{ height: '15rem', width: '25rem' }}
+        />
+      </a>
+      <p>{abstract}</p>
+    </div>
+  );
 }
 
-export default Article
+export default Article;
