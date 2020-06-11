4314d6ddc5108699fb182230fcbf42b60baf21fd
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-11 13:04:04 -0700

    Created MainStory component. Imported and rendered in Jumbotron component.

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import MainStory from "./MainStory";
 
 function Jumbotron() {
   return (
     <div>
       <div>Jumbotron</div>
+      <MainStory />
     </div>
   );
 }
---
+++ src/components/MainStory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainStory() {
+  return (
+    <div>
+      <div>Main Story</div>
+    </div>
+  );
+}
+
+export default MainStory;
