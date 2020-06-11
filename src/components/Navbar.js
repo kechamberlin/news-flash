6366f327de3549c6255d0b271f8540bffad81402
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-11 14:48:35 -0700

    Added makeshift Nav component from react-bootstrap to Navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import Nav from 'react-bootstrap/Nav';
 
 function MyNavbar() {
   return (
     <div>
-      <div>Navbar</div>
+      <Nav
+        activeKey='/home'
+        onSelect={selectedKey => alert(`selected ${selectedKey}`)}
+      >
+        <Nav.Item>
+          <Nav.Link eventKey='link-1'>Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey='link-2'>Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey='link-3'>Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey='link-4'>Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey='link-5'>Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey='link-6'>Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey='link-7'>Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey='link-8'>Link</Nav.Link>
+        </Nav.Item>
+      </Nav>
     </div>
   );
 }
