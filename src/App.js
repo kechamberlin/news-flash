e355239f44bbf27f45e495ecb67e03dff0f62f3a
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-11 12:31:20 -0700

    Created basic Footer component, imported and added to return statement in App.js.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import TopStories from './components/TopStories';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Navbar />
       <TopStories />
+      <Footer />
     </div>
   );
 }
