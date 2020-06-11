3813581db9afc778e8d1d16d76714903150e9276
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-11 14:45:15 -0700

    Imported Jumbotron, Container, Row, Column from react-bootstrap. Added corresponding imports to return statement.

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import MainStory from './MainStory';
 import MostPopular from './MostPopular';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function MyJumbotron() {
   return (
     <div>
-      <div>Jumbotron</div>
-      <MainStory />
-      <MostPopular />
+      <Container>
+        <Jumbotron>
+          <Row className='justify-content-md-center'>
+            <Col>
+              <MainStory />
+            </Col>
+            <Col xs lg='2'>
+              <MostPopular />
+            </Col>
+          </Row>
+        </Jumbotron>
+      </Container>
     </div>
   );
 }
