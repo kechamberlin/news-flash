288f7ebc9cbe7ab62bda54a2ef2f95e920ae58fc
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-12 12:09:23 -0700

    Moved TopStories JavaScript code to here, including useState, useEffect hooks; getStories function, and slice, map methods. Added method comments from TopStories to bottom of file below export for potential future references.

--- src/components/AmericanNews.js
+++ src/components/AmericanNews.js
@@ -1,18 +1,68 @@
-import React from 'react';
-import TopStories from "./TopStories";
-import Politics from "./Politics";
-import Opinion from "./Opinion";
+import React, { useState, useEffect } from 'react';
+import TopStories from './TopStories';
+import Politics from './Politics';
+import Opinion from './Opinion';
 
 function AmericanNews() {
+  const [story, setStory] = useState([]);
+
+  useEffect(() => {
+    getStories();
+  }, []);
+
+  const getStories = async () => {
+    const API_KEY = 'RT4G87zFK5XAmxjRnkqTtJyAD7Af42sZ';
+    const response = await fetch(
+      `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${API_KEY}`
+    );
+    const data = await response.json();
+    console.log(data.results);
+    setStory(data.results);
+  };
+
   return (
     <div>
       <div>American News</div>
-      <TopStories />
+      {story.slice(0, 5).map((news, index) => (
+        <TopStories
+          key={index}
+          title={news.title}
+          abstract={news.abstract}
+          image={news.multimedia[0].url}
+        />
+      ))}
       <Politics />
       <Opinion />
-      
     </div>
   );
 }
 
 export default AmericanNews;
+
+// THIS GRABS JUST THE 1 IMAGE I WANT
+//  ...getStories...
+//      ...console.log(data.results[0].multimedia[0].url);
+//      ...setStory(data.results[0].multimedia[0].url);
+//  ...
+//  <Article image={story} />
+
+// THIS SLICES THE ARRAY TO DISPLAY INDICES 0-5
+//  {story.slice(0,5).map((news, index) => (
+//      <Article
+//         key={index}
+//         title={news.title}
+//         abstract={news.abstract}
+//         image={news.multimedia[0].url}
+//      />
+// ))}
+
+//   THIS MAPS ALL THE STORIES AS ORIGINALLY SEEN
+//       {story.map(news => (
+//           <Article
+//           key={news.title}
+//           title={news.title}
+//           abstract={news.abstract}
+//           image={news.multimedia[0].url}
+//           url={news.url}
+//           />
+//       ))}
