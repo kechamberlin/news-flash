3082d9346f4a3f16e88f444dd2edd00a9faae6a7
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-12 12:15:06 -0700

    Changed fetched API data link to be US news.

--- src/components/AmericanNews.js
+++ src/components/AmericanNews.js
@@ -13,7 +13,7 @@ function AmericanNews() {
   const getStories = async () => {
     const API_KEY = 'RT4G87zFK5XAmxjRnkqTtJyAD7Af42sZ';
     const response = await fetch(
-      `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${API_KEY}`
+      `https://api.nytimes.com/svc/topstories/v2/us.json?api-key=${API_KEY}`
     );
     const data = await response.json();
     console.log(data.results);
