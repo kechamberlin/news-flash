6d2645f7d9bf36530f3460e502b987ba4e2301f4
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-12 12:56:17 -0700

    Created images folder and added New York Times branding logo from their api website. They request this logo is displayed on my project. Imported logo into Footer and rendered in img tag.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from "../images/nytimes-api.png"
 
 function Footer() {
   return (
     <div>
       <div>Footer</div>
+      <img src={logo} alt=""/>
     </div>
   );
 }
