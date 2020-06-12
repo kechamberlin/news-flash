5eef93b78fb1e40e372f13f48bb290812446239d
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-12 12:37:57 -0700

    Copy and pasted, like before, code into Business component, and displayed all appropriate business-related data.

--- src/components/Business.js
+++ src/components/Business.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import TopStories from './TopStories';
 
 function Business() {
-    return (
-        <div style={{ backgroundColor: 'blue' }}>
-            <div>Business News</div>
-        </div>
-    )
+  const [story, setStory] = useState([]);
+
+  useEffect(() => {
+    getStories();
+  }, []);
+
+  const getStories = async () => {
+    const API_KEY = 'RT4G87zFK5XAmxjRnkqTtJyAD7Af42sZ';
+    const response = await fetch(
+      `https://api.nytimes.com/svc/topstories/v2/business.json?api-key=${API_KEY}`
+    );
+    const data = await response.json();
+    console.log(data.results);
+    setStory(data.results);
+  };
+
+  return (
+    <div style={{ backgroundColor: 'blue' }}>
+      <div>Business News</div>
+      {story.slice(0, 5).map((news, index) => (
+        <TopStories
+          key={index}
+          title={news.title}
+          abstract={news.abstract}
+          image={news.multimedia[0].url}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default Business
+export default Business;
