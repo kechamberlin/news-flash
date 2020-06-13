68fe94c65169e408ae55395331bcbe84e337272a
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-12 20:05:12 -0700

    Added same state, effect, and function code from previous components. Changed API data section to opinion.

--- src/components/Opinion.js
+++ src/components/Opinion.js
@@ -2,10 +2,32 @@ import React, { useState, useEffect } from 'react';
 import StoryRow from './StoryRow';
 
 function Opinion() {
+  const [story, setStory] = useState([]);
+
+  useEffect(() => {
+    getStories();
+  }, []);
+
+  const getStories = async () => {
+    const API_KEY = 'RT4G87zFK5XAmxjRnkqTtJyAD7Af42sZ';
+    const response = await fetch(
+      `https://api.nytimes.com/svc/topstories/v2/opinion.json?api-key=${API_KEY}`
+    );
+    const data = await response.json();
+    console.log(data.results);
+    setStory(data.results);
+  };
+
   return (
     <div style={{ backgroundColor: 'yellow' }}>
       <div>Opinion</div>
-      <StoryRow />
+      {story.slice(0, 8).map(news => (
+        <StoryRow
+          key={news.title}
+          image={news.multimedia[0].url}
+          title={news.title}
+        />
+      ))}
     </div>
   );
 }
