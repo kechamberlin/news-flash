517e06b5f6f62ca88d8a41b6cc71fe0f5f890a6b
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-12 19:39:34 -0700

    Added state, useEffect, and getStories function. getStories is set to most viewed articles in the last day. Slice method used to extract the top 10 stories.

--- src/components/MostPopular.js
+++ src/components/MostPopular.js
@@ -2,10 +2,28 @@ import React, { useState, useEffect } from 'react';
 import MostPopularItem from './MostPopularItem';
 
 function MostPopular() {
+  const [story, setStory] = useState([]);
+
+  useEffect(() => {
+    getStories();
+  }, []);
+
+  const getStories = async () => {
+    const API_KEY = 'RT4G87zFK5XAmxjRnkqTtJyAD7Af42sZ';
+    const response = await fetch(
+      `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${API_KEY}`
+    );
+    const data = await response.json();
+    console.log(data.results);
+    setStory(data.results);
+  };
+
   return (
     <div>
-      <div>Trending</div>
-      <MostPopularItem />
+      <p>Trending Today</p>
+      {story.slice(0, 10).map(news => (
+        <MostPopularItem key={news.id} title={news.title} />
+      ))}
     </div>
   );
 }
