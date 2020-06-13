5fcb5457bd032b1a39fbad324a468972edea523f
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-13 15:35:53 -0700

    Wrapped Figure component in anchor tag that links to API url. Added url variable to destructured function.

--- src/components/StoryRow.js
+++ src/components/StoryRow.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Figure from 'react-bootstrap/Figure';
 
-function StoryRow({ image, title, caption }) {
+function StoryRow({ image, title, caption, url }) {
   return (
     <div>
-      <Figure className='text-center' style={{ width: '15rem' }}>
-        <Figure.Image width={171} height={180} alt={caption} src={image} />
-        <Figure.Caption as='h1'>
-          <strong>{title}</strong>
-        </Figure.Caption>
-      </Figure>
+      <a href={url} target='_blank' rel='noopener noreferrer'>
+        <Figure className='text-center' style={{ width: '15rem' }}>
+          <Figure.Image width={171} height={180} alt={caption} src={image} />
+          <Figure.Caption as='h1'>
+            <strong>{title}</strong>
+          </Figure.Caption>
+        </Figure>
+      </a>
     </div>
   );
 }
