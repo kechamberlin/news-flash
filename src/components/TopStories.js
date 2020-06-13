ab854465456f4c8cd8bff4a96d14700f4b336d5a
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-13 14:13:52 -0700

    Replaced react-bootstrap Card with react-bootstrap Figure component. Added caption variable to destructured function.

--- src/components/TopStories.js
+++ src/components/TopStories.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
+import Figure from 'react-bootstrap/Figure';
 
-function TopStories({ title, abstract, image, url }) {
+function TopStories({ title, abstract, image, caption, url }) {
   return (
     <div>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant='top' src={image} />
-        <Card.Body>
-          <Card.Title>{title}</Card.Title>
-          <Card.Text>{abstract}</Card.Text>
-        </Card.Body>
-      </Card>
+      <Figure className='text-center' style={{ width: '18rem' }}>
+        <Figure.Image width={200} height={180} alt={caption} src={image} />
+        <Figure.Caption as='h1'>
+          <strong>{title}</strong>
+        </Figure.Caption>
+        <Figure.Caption style={{ fontSize: '12px' }}>{abstract}</Figure.Caption>
+      </Figure>
     </div>
   );
 }
