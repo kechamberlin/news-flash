8eec0353be428caed7b91d63ac4a9e491d5fa289
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-13 12:20:55 -0700

    Imported react-bootstrap Card component and replaced jsx elements with Card.

--- src/components/OpinionStoryRow.js
+++ src/components/OpinionStoryRow.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 function StoryRow({ title, byline }) {
   return (
     <div>
-      <p>{title}</p>
-      <p>{byline.substring(3)}</p>
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Subtitle className='mb-2 text-muted'>
+            {byline.substring(3)}
+          </Card.Subtitle>
+          <Card.Text>{title}</Card.Text>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
