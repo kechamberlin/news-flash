603b6f1c3d3b521615a523cde47d779375d04d63
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-12 20:01:29 -0700

    Imported useState, useEffect. Added state, effect, and getStories function set to politics section. Sliced first 8 articles from array.

--- src/components/Politics.js
+++ src/components/Politics.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import StoryRow from './StoryRow';
 
 function Politics() {
+  const [story, setStory] = useState([]);
+
+  useEffect(() => {
+    getStories();
+  }, []);
+
+  const getStories = async () => {
+    const API_KEY = 'RT4G87zFK5XAmxjRnkqTtJyAD7Af42sZ';
+    const response = await fetch(
+      `https://api.nytimes.com/svc/topstories/v2/politics.json?api-key=${API_KEY}`
+    );
+    const data = await response.json();
+    console.log(data.results);
+    setStory(data.results);
+  };
+
   return (
-    <div  style={{ backgroundColor: 'orange' }}>
+    <div style={{ backgroundColor: 'orange' }}>
       <div>Politics</div>
-      <StoryRow />
+      {story.slice(0, 8).map(news => (
+        <StoryRow key={news.title} image={news.multimedia[0].url} title={news.title} />
+      ))}
     </div>
   );
 }
