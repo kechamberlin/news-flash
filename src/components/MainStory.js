e283ed66572d1e7899e4fac2eeeac8125d0bfb27
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-12 19:13:31 -0700

    Added useState, useEffect Hooks; getStories function set to the home section. Added slice method to the mapped MainStoryItem component so it will only grab the first object's data.

--- src/components/MainStory.js
+++ src/components/MainStory.js
@@ -2,9 +2,32 @@ import React, { useState, useEffect } from 'react';
 import MainStoryItem from './MainStoryItem';
 
 function MainStory() {
+  const [story, setStory] = useState([]);
+
+  useEffect(() => {
+    getStories();
+  }, []);
+
+  const getStories = async () => {
+    const API_KEY = 'RT4G87zFK5XAmxjRnkqTtJyAD7Af42sZ';
+    const response = await fetch(
+      `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${API_KEY}`
+    );
+    const data = await response.json();
+    console.log(data.results);
+    setStory(data.results);
+  };
+
   return (
     <div>
-      <div>Main Story</div>
+      {story.slice(0, 1).map((news, index) => (
+        <MainStoryItem
+          key={index}
+          image={news.multimedia[0].url}
+          title={news.title}
+          abstract={news.abstract}
+        />
+      ))}
     </div>
   );
 }
