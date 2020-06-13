bc9f226c721c0d3d71cc975332bc1f8a9a5544e9
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-13 12:15:26 -0700

    Imported Card component and replaced jsx elements with react-bootstrap Card.

--- src/components/StoryRow.js
+++ src/components/StoryRow.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 function StoryRow({ image, title }) {
   return (
     <div>
-      <img style={{ height: '5rem', width: '5rem' }} src={image} alt='' />
-      <p>{title}</p>
+      {/* <img style={{ height: '5rem', width: '5rem' }} src={image} alt='' />
+      <p>{title}</p> */}
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant='top' src={image} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
