452bb50bc09cf506b4572417b120b75614e6a2a6
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-12 19:17:29 -0700

    Imported useState, useEffect, MostPopularItem. Added MPI component to return statement.

--- src/components/MostPopular.js
+++ src/components/MostPopular.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import MostPopularItem from './MostPopularItem';
 
 function MostPopular() {
   return (
     <div>
       <div>Trending</div>
+      <MostPopularItem />
     </div>
   );
 }
