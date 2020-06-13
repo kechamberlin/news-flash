09c3772385fb6d2cdb2852c717ed662206e1b886
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-13 14:22:45 -0700

    Imported Container, Row, and Col components from react-bootstrap.

--- src/components/SciTech.js
+++ src/components/SciTech.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import TopStories from './TopStories';
 import Technology from './Technology';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function SciTech() {
   const [story, setStory] = useState([]);
--- src/components/World.js
+++ src/components/World.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import TopStories from './TopStories';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function World() {
   const [story, setStory] = useState([]);
