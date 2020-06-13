53263765cfb61a176d093610c4dc454e0f845c3e
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-13 15:27:52 -0700

    Wrapped Figure component in anchor tag that links to API url.

--- src/components/TopStories.js
+++ src/components/TopStories.js
@@ -4,13 +4,17 @@ import Figure from 'react-bootstrap/Figure';
 function TopStories({ title, abstract, image, caption, url }) {
   return (
     <div>
-      <Figure className='text-center' style={{ width: '18rem' }}>
-        <Figure.Image width={200} height={180} alt={caption} src={image} />
-        <Figure.Caption as='h1'>
-          <strong>{title}</strong>
-        </Figure.Caption>
-        <Figure.Caption style={{ fontSize: '12px' }}>{abstract}</Figure.Caption>
-      </Figure>
+      <a href={url} target='_blank' rel='noopener noreferrer'>
+        <Figure className='text-center' style={{ width: '18rem' }}>
+          <Figure.Image width={200} height={180} alt={caption} src={image} />
+          <Figure.Caption as='h1'>
+            <strong>{title}</strong>
+          </Figure.Caption>
+          <Figure.Caption style={{ fontSize: '12px' }}>
+            {abstract}
+          </Figure.Caption>
+        </Figure>
+      </a>
     </div>
   );
 }
