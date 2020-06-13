0771f9fa9dc3224625abab91957b330171a6ea62
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-13 15:07:04 -0700

    Imported Container, Row, Col components from react-bootstrap.

--- src/components/Technology.js
+++ src/components/Technology.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import StoryRow from './StoryRow';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Technology() {
   const [story, setStory] = useState([]);
