7aa82325948a015dee760c46b032182d55c185ff
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-13 15:22:50 -0700

    Imported Container, Row, Col, and Image components from react-bootstrap. Replaced img tag with imported bootstrap components. Added disclaimer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import logo from "../images/nytimes-api.png"
+import logo from '../images/nytimes-api.png';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 
 function Footer() {
   return (
     <div>
-      <div>Footer</div>
-      <img src={logo} alt=""/>
+      <Container className='text-center'>
+        <Row>
+          <Col>
+            <a
+              href='https://developer.nytimes.com/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <Image src={logo} fluid />
+            </a>
+            <div>
+              <strong>Disclaimer:</strong> The New York Times does not promote
+              or endorse this webpage or any third party, nor the causes, ideas,
+              web sites, products, or services of this webpage or any third
+              party.
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
