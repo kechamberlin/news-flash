ccf4581a2546f24c554915f239a4f6b34450e6e2
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-19 14:36:55 -0700

    Imported Image component from react-bootstrap. Replaced img tag with Image component.

--- src/components/MainStoryItem.js
+++ src/components/MainStoryItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Image from 'react-bootstrap/Image';
 
 function MainStoryItem({ image, title, abstract }) {
   return (
     <div>
-      <img src={image} alt='' style={{ height: '20rem', width: '40rem' }} />
-      <p>{title}</p>
+      <Image src={image} fluid className='main-story-image' />
+      <h4>{title}</h4>
       <p>{abstract}</p>
     </div>
   );
