bdb3329b24929f335aa92d2aa4f0a3a28e94a56a
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-20 13:29:49 -0700

    Added className main-story-image to Image component.

--- src/components/MainStoryItem.js
+++ src/components/MainStoryItem.js
@@ -4,7 +4,7 @@ import Image from 'react-bootstrap/Image';
 function MainStoryItem({ image, title, abstract }) {
   return (
     <div>
-      <Image src={image} fluid />
+      <Image src={image} fluid className='main-story-image' />
       <h4>{title}</h4>
       <p>{abstract}</p>
     </div>
