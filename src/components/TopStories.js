a45a19b4eb7cd5eebf4d8d8dc4702ffcf24c3975
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-23 15:52:30 -0700

    Replaced Figure component with Media component from react-bootstrap. Commented out Figure import, render; May use this format for media responsiveness. Added className article to Media component.

--- src/components/TopStories.js
+++ src/components/TopStories.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import Figure from 'react-bootstrap/Figure';
+import Media from 'react-bootstrap/Media';
+// import Figure from 'react-bootstrap/Figure';
 
 function TopStories({ title, abstract, image, caption, url }) {
   return (
     <div>
       <a href={url} target='_blank' rel='noopener noreferrer'>
-        <Figure className='text-center' style={{ width: '18rem' }}>
+        <Media className='article'>
+          <img
+            width={250}
+            height={150}
+            className='mr-3'
+            src={image}
+            alt={caption}
+          />
+          <Media.Body>
+            <h5>{title}</h5>
+            <p>{abstract}</p>
+          </Media.Body>
+        </Media>
+
+        {/* <Figure className='text-center' style={{ width: '18rem' }}>
           <Figure.Image width={200} height={180} alt={caption} src={image} />
           <Figure.Caption as='h1'>
             <strong>{title}</strong>
@@ -13,7 +28,7 @@ function TopStories({ title, abstract, image, caption, url }) {
           <Figure.Caption style={{ fontSize: '12px' }}>
             {abstract}
           </Figure.Caption>
-        </Figure>
+        </Figure> */}
       </a>
     </div>
   );
