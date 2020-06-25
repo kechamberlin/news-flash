a7f2f9eead39c078e3e168cec2c778110cd7bcf9
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-06-25 14:16:54 -0700

    Renamed SciTech.js file to Science.

---
+++ src/components/Science.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import TopStories from './TopStories';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function Science() {
+  const [story, setStory] = useState([]);
+
+  useEffect(() => {
+    getStories();
+  }, []);
+
+  const getStories = async () => {
+    const API_KEY = 'RT4G87zFK5XAmxjRnkqTtJyAD7Af42sZ';
+    const response = await fetch(
+      `https://api.nytimes.com/svc/topstories/v2/science.json?api-key=${API_KEY}`
+    );
+    const data = await response.json();
+    console.log(data.results);
+    setStory(data.results);
+  };
+
+  return (
+    <div id='science' style={{ backgroundColor: 'whitesmoke' }}>
+      <h1 className='section-heading'>Science</h1>
+      <Container>
+        <Row>
+          <Col lg={8}>
+            {story.map((news, index) => (
+              <TopStories
+                key={index}
+                title={news.title}
+                abstract={news.abstract}
+                image={news.multimedia[0].url}
+                url={news.url}
+              />
+            ))}
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default Science;
